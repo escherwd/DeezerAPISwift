d22aea5d7177c0a8e43321d07834a2a4ff78d775
Escher WD <escherwd@yahoo.com>
2025-04-15 20:04:31 -0700

    Get fav track ids without getTracks

--- Sources/DeezerAPISwift/Methods/getUserFavTracks.swift
+++ Sources/DeezerAPISwift/Methods/getUserFavTracks.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 extension DeezerAPI {
-
-    public func getUserFavTracks() async throws -> [DeezerTrack] {
-
+    
+    public func getUserFavTrackIds() async throws -> [DeezerFavEntry] {
+        
         // Get the IDs first
         let trackIds: getFavoriteIdsResponse = try await self.requestGwLight(
             "song.getFavoriteIds",
@@ -19,17 +19,30 @@ extension DeezerAPI {
                 "start": 0,
             ]
         )
-
+        
         // No tracks
         if trackIds.count == 0 { return [] }
+        
+        return trackIds.data.map {
+            DeezerFavEntry(id: Int($0.SNG_ID)!, dateFavorite: $0.DATE_FAVORITE)
+        }
+        
+        
+    }
+
+    
+    public func getUserFavTracks() async throws -> [DeezerTrack] {
+
+        // Get the track ids from the function above
+        let trackIds = try await self.getUserFavTrackIds()
 
         var tracks: [DeezerTrack] = []
 
-        for chunk in trackIds.data.chunks(ofCount: 500) {
+        for chunk in trackIds.chunks(ofCount: 500) {
             tracks.append(
                 contentsOf: try await self.getTracks(
-                    chunk.map { Int($0.SNG_ID)! },
-                    favTimestamps: chunk.map(\.DATE_FAVORITE)
+                    chunk.map(\.id),
+                    favTimestamps: chunk.map(\.dateFavorite)
                 )
             )
         }
---
+++ Sources/DeezerAPISwift/Types/FavEntry.swift
@@ -0,0 +1,13 @@
+//
+//  FavEntry.swift
+//  DeezerAPISwift
+//
+//  Created by Escher Wright-Dykhouse on 4/15/25.
+//
+
+public struct DeezerFavEntry: Decodable {
+    
+    let id: Int
+    let dateFavorite: Int
+    
+}
